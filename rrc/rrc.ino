f4e3d551488cefb3fe6886a6f3d20bd148d9ba58
Jim <james@colderwood.com>
2021-12-20 21:34:10 +0000

    2 Radio PORT control

--- rrc/rrc.ino
+++ rrc/rrc.ino
@@ -1,91 +1,78 @@
-/**
- * DMTF RELAY CONTROL Jim M0ZAH Ported
- * 8 relays with password protection
- * 1 enable output, 0 disable output
- * EG: 31 will enable relay 3, 30 will disable relay 3
+/*
+ * DMTF RELAY CONTROL Jim M0ZAH
+ * Based on G8SDU ASM
+ * 2021
+ */
+
+/*
+ * PORT B 9 - 13 DTMF decoder; active high
+ * - 9 D1
+ * - 10 D2
+ * - 11 D3
+ * - 12 D4
+ * - 13 Tone detected
+ * PORT C 0 Tone detect lamp
+ * PORT C 1 Success lamp
+ * PORT D 1 - 4 Radio 1
+ * PORT D 5 - 7 PORT B 8 Radio 2
  */
 
 // Options
-#define DTMF_DELAY 1000 // Delay before next digit in milliseconds
+#define DTMF_DELAY 500 // Delay before next digit in milliseconds
+#define END_DIGIT 0xC // (#)
 #define PASSCODE 1111 // Password
-#define RELAY_1 0x1 // Relay 1
-#define RELAY_2 0x2 // Relay 2
-#define RELAY_3 0x3 // Relay 3
-#define RELAY_4 0x4 // Relay 4
-#define RELAY_5 0x5 // Relay 5
-#define RELAY_6 0x6 // Relay 6
-#define RELAY_7 0x7 // Relay 7
-#define RELAY_8 0x8 // Relay 8
+#define START_DIGIT 0xB // (*)
 
 // MASKS
-#define DTMF_DETECT 0x10 // DTMF DETECT LINE
-#define DTMF_MASK 0xF // DTMF Digits
+#define DTMF_DETECT 0x20 // DTMF DETECT LINE
+#define DTMF_MASK 0x1E // DTMF Digits
 
 int DTMF_CODE; // Digit store
 int cc = 0; // Digit counter
-int input[5] = {0}; // Input array
+int input[7] = {0}; // Input array
+// [*,p,p,p,p,1/2/3,CMD,CMD2]
 
 void setup()
 {
   DDRB = 0x0; // Set port B to inputs
   DDRD = 0xFE; // Set port D to 1-7 OUTPUTS
+  DDRC = 0x1F; // // Set port C to 0-5 OUTPUTS
   PORTD = 0x0; // Outputs low 
   PINB = 0x0; // PULLUPS LOW
 }
 
 void loop()
 {
-  while ((PINB & DTMF_DETECT) == DTMF_DETECT)
+  if ((PINB & DTMF_DETECT) == DTMF_DETECT)
   { // When we receive a valid digit
-    DTMF_CODE = (PINB & DTMF_MASK);
+    PORTC = 0x1; // Tone detect lamp 
+    DTMF_CODE = ((PINB & DTMF_MASK) >> 1);
     input[cc] = DTMF_CODE; // Store our current digit into array
 
-    if (cc >= 5) // If we've received at least 6 digits
+    if (cc >= 7) // If we've received at least 8 digits
     {
+      if (input[0] != START_DIGIT) // Check the command started with *
+      {
+        // TODO: command is malformed
+      } else {
       int pass = 0;
       for (int i = 0; i < 4; i++) // Process the password
       {
         pass = pass*10;
-        pass = pass + input[i];
+        pass = pass + input[i+1];
       }
       if (pass == PASSCODE) // If the password is correct
       {
-        switch (input[4]) // Relay selection
-        {
-          case RELAY_1:
-          (input[5]) ? PIND = PIND | 0x2 : PIND = 0;
-          break;
-          case RELAY_2:
-          (input[5]) ? PIND = PIND | 0x4 : PIND = 0;
-          break;
-          case RELAY_3:
-          (input[5]) ? PIND = PIND | 0x8 : PIND = 0;
-          break;
-          case RELAY_4:
-          (input[5]) ? PIND = PIND | 0x10 : PIND = 0;
-          break;
-          case RELAY_5:
-          (input[5]) ? PIND = PIND | 0x20 : PIND = 0;
-          break;
-          case RELAY_6:
-          (input[5]) ? PIND = PIND | 0x40 : PIND = 0;
-          break;
-          case RELAY_7:
-          (input[5]) ? PIND = PIND | 0x80 : PIND = 0;
-          break;
-          case RELAY_8:
-          break;
-          default:
-          // invalid selection
-          _delay_ms(500);
-          break;
-        }
+        PORTC = 0x2; // Success lamp to show access granted
       }
       cc = -1; // Reset digit counter
       DTMF_CODE = 0;
     }
-
+   }
      _delay_ms(DTMF_DELAY); // Max length of each digit
      ((PINB & DTMF_DETECT) == DTMF_DETECT) ? cc = 0 : cc++; // If we're still receiving a digit invalidate it
+  }
+  if (PORTC != 0x0){ // Reset status lamps
+    PORTC = 0x0;
   }
 }
